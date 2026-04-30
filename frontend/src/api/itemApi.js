3357102b088925b654ebd75a7c8aa8d58f63c7c0
ynimneth <ynimneth@gmail.com>
2026-04-30 14:10:58 +0530

    Updated

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
 });
 
 export const getItems = () => API.get("/items");
